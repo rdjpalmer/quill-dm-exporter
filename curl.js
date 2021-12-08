ce3eef2e5437661515f3eed63871ed4deff0dbb7
Richard Palmer <rich@rdjpalmer.com>
2021-12-08 09:36:39 +0000

    Add missing licence

--- curl.js
+++ curl.js
@@ -1,3 +1,30 @@
+/**
+ * Copied from https://github.com/do-web/curl-request/blob/f2c83247436c5de1b9e6eb8b96ae71f1030e2592/index.js
+ * as I couldn't get the damn thing to install with `npm install curl-request` 🤷‍♀️
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Dominik Weber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 const Curl = require("node-libcurl").Curl;
 const Net = require("net");
 const querystring = require("querystring");
